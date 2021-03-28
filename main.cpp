0beab7eea43e76e35ccf3e57df599d240e7ba9c9
Maksim <maksim.mystafa@gmail.com>
2021-03-28 17:26:58 +0300

    Task 3-Problem

--- main.cpp
+++ main.cpp
@@ -1,5 +1,40 @@
 #include "Task1.h"
 #include "Task2.h"
+#include <condition_variable>
+
+std::mutex mutex3;
+std::condition_variable conditionVar;
+std::vector<int> PartProductsOfVectors;  //общий вектор произведений элементов векторов
+std::vector<int> PartSums(10);
+
+void ScalarProductVectors(std::vector<int>& vec1, std::vector<int>& vec2, int start, int end, int i)
+{
+  std::unique_lock<std::mutex> uniqueLck(mutex3);
+  int product;
+  for (int i = start; i < end; i++)
+  {
+    product = vec1[i] * vec2[i];
+    PartProductsOfVectors.insert(PartProductsOfVectors.begin() + i, product);
+  }
+  std::cout << "Thread #" << i << " is waiting" << std::endl;
+
+  conditionVar.wait(uniqueLck);
+  std::cout << "Thread #" << i << " is continuing to work after waiting" << std::endl;
+
+  int sum = 0;
+  for (int i = start; i < end; i++)
+  {
+    sum += PartProductsOfVectors[i];
+  }
+
+  PartSums.emplace_back(sum);
+  std::cout << "Thread #" << i << " has ended to work" << std::endl;
+}
+
+void Go()
+{
+  conditionVar.notify_all();
+}
 
 int main()
 {
@@ -19,6 +54,37 @@ int main()
   /**************************************************************************************/
   std::cout << "Task 2" << std::endl;
   auto vectorElemsProduct = Start2(1000);
+  sum_of_elems = 0;
+  for (auto& n : vectorElemsProduct)
+    sum_of_elems += n;
+  std::cout << "Scalar product of two vectors = " << sum_of_elems << std::endl;
+  std::cout << std::endl;
+  /**************************************************************************************/
+
+  // TASK 3
+  /**************************************************************************************/
+  std::cout << "Task 3" << std::endl;
+  std::vector<int> vec1(1000, 3);
+  std::vector<int> vec2(1000, 5);
+
+  std::array<std::thread, 10> threads;
+
+  int step = 100;
+  int start = 0;
+  int end = step;
+
+  for (int i = 0; i < 10; i++)
+  {
+    threads[i] = std::thread(ScalarProductVectors, std::ref(vec1), std::ref(vec2), start, end, i);
+    start += step;
+    end += step;
+  }
+
+  Go();
+
+  for (auto& th : threads)
+    th.join();
+
   sum_of_elems = 0;
   for (auto& n : vectorElemsProduct)
     sum_of_elems += n;
