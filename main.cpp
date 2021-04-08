0d6281184602f5a66556a7bb3d6753e7db990ee1
Ra <galievarailina@gmail.com>
2021-04-08 15:00:44 +0300

    Edit

--- main.cpp
+++ main.cpp
@@ -7,96 +7,76 @@
 #include <array>
 #include <future>
 std::mutex mutex;
-void saveSumInVector(std::vector<int>& sums, std::vector<int>& data, int start, int finish)
+int saveSumInVector(const std::vector<int>& data, int start, int finish)
 {
   int sum = 0;
   for (int i = start; i < finish; i++)
   {
     sum += data[i];
   }
-  std::lock_guard<std::mutex> guard(mutex);
-  sums.push_back(sum);
+  return sum;
 }
 
 void task_1()
 {
-  std::vector<int> sums;
+  std::vector<int> sums(10);
   std::vector<int> vector(1000, 1);
-  std::vector<std::thread> threads;
   for (int i = 0; i < 10; i++)
-    threads.push_back(std::thread(saveSumInVector, std::ref(sums), std::ref(vector), i * 100, (i + 1) * 100));
-  for (auto& th : threads)
-    th.join();
+    sums[i] = std::async(saveSumInVector, std::ref(vector), i * 100, (i + 1) * 100).get();
   int sum = std::accumulate(sums.begin(), sums.end(), 0);
   std::cout << "task 1: " << sum << std::endl;
 }
 
-void multiplyElements(std::vector<int>& result, std::vector<int>& vect1, std::vector<int>& vect2, int start, int finish)
+void multiplyElementsAndSave(std::vector<int>& savingVector, const std::vector<int>& vect1,
+                             const std::vector<int>& vect2, int start, int finish)
 {
   for (int i = start; i < finish; i++)
   {
-    result[i] = vect1[i] * vect2[i];
+    savingVector[i] = vect1[i] * vect2[i];
   }
-}
-
-int sum(std::vector<int>& data, int start, int finish)
-{
-  int result = 0;
-  for (int i = start; i < finish; i++)
-  {
-    result += data[i];
-  }
-  return result;
 }
 
 void task_2()
 {
   std::vector<int> v1(1000, 1);
   std::vector<int> v2(1000, 2);
-  std::vector<int> multiplyResults;
+  std::vector<int> multiplyResults(1000);
   std::vector<std::thread> threads;
+  std::vector<int> sums(10);
   //сохраняю произведения
   for (int i = 0; i < 10; i++)
   {
-    threads.push_back(
-        std::thread(multiplyElements, std::ref(multiplyResults), std::ref(v1), std::ref(v2), i * 100, (i + 1) * 100));
+    threads.push_back(std::thread(multiplyElementsAndSave, std::ref(multiplyResults), std::ref(v1), std::ref(v2),
+                                  i * 100, (i + 1) * 100));
   }
   for (auto& th : threads)
     th.join();
 
-  std::vector<std::future<int>> sums;
   //Вычисляю суммы разных отрезков произведений в отдельных потоках
   for (int i = 0; i < 10; i++)
   {
-    sums[i] = std::async(sum, std::ref(multiplyResults), i * 100, (i + 1) * 100);
+    sums[i] = std::async(saveSumInVector, std::ref(multiplyResults), i * 100, (i + 1) * 100).get();
   }
-  int sum = 0;
-
-  //Вычисляю сумму
-  for (int i = 0; i < 10; i++)
-  {
-    sum += sums[i].get();
-  }
-
+  int sum = std::accumulate(sums.begin(), sums.end(), 0);
   std::cout << "task 2: " << sum << std::endl;
 }
 
 std::condition_variable cv;
-int multiplyAndGetSum(std::vector<int>& vect1, std::vector<int>& vect2, int start, int finish)
+void multiplyAndSaveSum(std::vector<int>& sumVector, int savingNumber, const std::vector<int>& vect1,
+                        const std::vector<int>& vect2, int start, int finish)
 {
-  std::vector<int> sums;
+  int n = finish - start;
+  std::vector<int> currentMultiplyResults(n);
+  int counter = 0;
   for (int i = start; i < finish; i++)
   {
-    sums[i] = vect1[i] * vect2[i];
+    currentMultiplyResults[counter] = vect1[i] * vect2[i];
+    counter++;
   }
   std::unique_lock<std::mutex> lck(mutex);
   cv.wait(lck);
-  int result = 0;
-  for (int i = start; i < finish; i++)
-  {
-    result += sums[i];
-  }
-  return result;
+  int sum = std::accumulate(currentMultiplyResults.begin(), currentMultiplyResults.end(), 0);
+  sumVector[savingNumber] = sum;
 }
 
 void go()
@@ -108,16 +88,18 @@ void task_3()
 {
   std::vector<int> v1(1000, 1);
   std::vector<int> v2(1000, 2);
-  std::vector<std::future<int>> sums;
-  for (int i = 0; i < 10; ++i)
+  std::vector<int> sums(10);
+  std::vector<std::thread> threads;
+  for (int i = 0; i < 10; i++)
   {
-    sums[i] = std::async(multiplyAndGetSum, std::ref(v1), std::ref(v2), i * 100, (i + 1) * 100);
+    threads.push_back(
+        std::thread(multiplyAndSaveSum, std::ref(sums), i, std::ref(v1), std::ref(v2), i * 100, (i + 1) * 100));
   }
   std::this_thread::sleep_for(std::chrono::milliseconds(3000));
   go();
-  int sum = 0;
-  for (int i = 0; i < 10; i++)
-    sum += sums[i].get();
+  for (auto& th : threads)
+    th.join();
+  int sum = std::accumulate(sums.begin(), sums.end(), 0);
   std::cout << "task 3: " << sum << std::endl;
 }
 
